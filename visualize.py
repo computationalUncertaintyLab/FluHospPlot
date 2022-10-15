5381102a360415023a0953336a9e231f1c531985
wenxuanye <wey420@lehigh.edu>
2022-10-15 17:07:04 -0400

    Flu Hosp data generated on  2022-10-15

--- visualize.py
+++ visualize.py
@@ -24,7 +24,9 @@ class visualize(object):
         ax.plot( plotdata, lw=2,alpha=0.40,color="blue" )
         # rotate the xtick labels
         ax.scatter(dates,hosps[::-1][::4][::-1],s=10,color="blue",alpha=0.8)
-        plt.xticks(rotation=-90,fontsize = 2)
+        plt.xticks(rotation=-90)
+        # make the x_axis labels smaller
+        ax.xaxis.set_tick_params(labelsize=1)
         #label points 5 weeks in the past
         hospsPast5 = hosps[-5:]
         datesPast5 = plotdata.index[-5:]
